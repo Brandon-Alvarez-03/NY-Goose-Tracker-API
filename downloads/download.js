bda91565d1bf1a3b5472a575d8c31e31adfb769e
“Brandon <balvarez325@gmail.com>
2022-10-20 10:42:02 -0400

    fix full state download incrementer

--- downloads/download.js
+++ downloads/download.js
@@ -7,45 +7,61 @@ const speciesCode = `cangoo`
 
 let regionCodeCounter = 1
 let regionCode = `US-NY-00`
+let gooseArray = []
 
 
-while (regionCodeCounter <= 123) {
-  if (regionCodeCounter > 99) {
-    regionCode = 'US-NY-'
-  } else if (regionCodeCounter > 9) {
-    regionCode = 'US-NY-0'
-  }
   // console.log(`${regionCode + regionCodeCounter.toString()}`)
   let newRegionCode = `${regionCode + regionCodeCounter.toString()}`
 
   console.log(newRegionCode)
 
-    let geeseConfig = {
-      method: 'get',
-      url: `https://api.ebird.org/v2/data/obs/${newRegionCode}/recent/${speciesCode}`,
-      headers: {
-        'X-eBirdApiToken': `${birdApiToken}`
-      }
-    };
-  axios(geeseConfig)
-    .then(function (response) {
-      let newData = response.data
-      // console.log(newRegionCode)
-      // console.log(newData)
-      console.log(newRegionCode)
-      // newData.forEach(data => fsPromises.appendFile("./geese-NYS-complete.json", JSON.stringify(data) + "," + "\n"))
-          })
-    .catch(function (error) {
-      console.log(error);
-    })
+  let geeseConfig = {
+    method: 'get',
+    url: `https://api.ebird.org/v2/data/obs/${newRegionCode}/recent/${speciesCode}`,
+    headers: {
+      'X-eBirdApiToken': `${birdApiToken}`
+    }
+  };
+  async function fetchGeese(index, config) {
+    const response = await axios(config)
+    const newData = response.data
+    console.log(index)
+    console.log(newRegionCode)
+    // console.log(newData)
+    // console.log(newRegionCode)
+    // newData.forEach(data => fsPromises.appendFile("./geese-NYS-complete.json", JSON.stringify(data) + "," + "\n"))
+    gooseArray[index] = response.data
+  }
 
-    let usNyConfig = {
-      method: 'get',
-      url: `https://api.ebird.org/v2/ref/region/info/${newRegionCode}?fmt=json`,
-      headers: { 
-        'X-eBirdApiToken': `${birdApiToken}`
+  async function changeRegion() {
+    let i = 0
+    while (regionCodeCounter <= 123) {
+      if (regionCodeCounter > 99) {
+        regionCode = 'US-NY-'
+        newRegionCode = `${regionCode + regionCodeCounter.toString()}`
+      } else if (regionCodeCounter > 9) {
+        regionCode = 'US-NY-0'
+        newRegionCode = `${regionCode + regionCodeCounter.toString()}`
+      } else {
+        regionCode = 'US-NY-00'
+        newRegionCode = `${regionCode + regionCodeCounter.toString()}`
       }
-    };
+      await fetchGeese(i, geeseConfig)
+      i++
+      regionCodeCounter += 2
+    }
+    console.log(gooseArray)
+  }
+  changeRegion()
+
+
+    // let usNyConfig = {
+    //   method: 'get',
+    //   url: `https://api.ebird.org/v2/ref/region/info/${newRegionCode}?fmt=json`,
+    //   headers: { 
+    //     'X-eBirdApiToken': `${birdApiToken}`
+    //   }
+    // };
     // NY region --> US-NY this one just says 'new, York, united states'
     // Counties in NY --> US-NY-001 through US-NY-123 only odd numbers these provide one individual object with result i.e. 'Yates, New York, United States'
     // need to write these into a file
@@ -61,9 +77,7 @@ while (regionCodeCounter <= 123) {
     // .catch(function (error) {
     //     console.log(error);
     // });
-  regionCodeCounter += 2
-  if (regionCodeCounter > 123) {break}
-}
+
 
 // run this to see species codes to populate new json data with diff bird species
 // const taxonomyConfig = {
